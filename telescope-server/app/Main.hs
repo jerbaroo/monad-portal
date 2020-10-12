7a1d0ece12c4915687044e5e1b6bdbf9b7d67407
jerbaroo <jerbaroo.work@pm.me>
2020-10-12 17:56:16 +0100

    Add debug string

--- telescope-server/app/Main.hs
+++ telescope-server/app/Main.hs
@@ -23,3 +23,25 @@ main = do
   liftIO $ runTFile $ mapM T.set [ Person "John" 3 ]
   print =<< (liftIO $ runTFile $ T.viewTable Person{})
   Server.run 3001
+
+{-
+To test websocket in browser:
+
+function newws() {
+  var ws = new WebSocket("ws://localhost:3001/update");
+  ws.onopen = function() { console.log("Opened"); };
+  ws.onmessage = function (evt) {
+     var received_msg = evt.data;
+     console.log("Message received...");
+     console.log(received_msg);
+  };
+  ws.onclose = function() {
+     // websocket is closed.
+     console.log("Connection closed");
+  };
+  return ws;
+};
+var ws = newws();
+ws.send("(TableKey \"Person\",RowKey (KeyOne (PString \"John\")))");
+
+-}
