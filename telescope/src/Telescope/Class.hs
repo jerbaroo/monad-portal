dc7d07e4c0d166fb7255a4a2a2d06daf64f359e5
jerbaroo <jerbaroo.work@pm.me>
2020-11-08 11:47:36 +0000

    Rewrite telescope/../Class.hs

--- telescope/src/Telescope/Class.hs
+++ telescope/src/Telescope/Class.hs
@@ -10,7 +10,9 @@
 module Telescope.Class (module Telescope.Class, PrimaryKey) where
 
 import           Control.Monad     ( void )
+import           Data.Functor      ( (<&>) )
 import qualified Data.Map         as Map
+import qualified Data.Set         as Set
 import qualified Telescope.Store  as Store
 import qualified Telescope.Table  as Table
 import           Telescope.Table   ( PrimaryKey )
@@ -20,70 +22,86 @@ class ToFromF f a where
   toF   :: a -> f a
   fromF :: f a -> a
 
--- | Defines primitive operations to interact with a data source.
---
--- All operations are based on 'Table.Row' and similar data types.
+-- | 'Table.Row'-based operations for interacting with a data source.
+-- TODO: Use Set instead of list for key types.
 class (Applicative f, Monad m, forall a. ToFromF f a)
   => Telescope m f | m -> f where
   escape :: f a -> m a
   escape = pure . fromF
   enter  :: a -> m (f a)
   enter = pure . toF
+  perform :: f (m ()) -> m ()
 
-  -- | View a table row in a data source.
-  --
-  -- 'Maybe' to indicate the entry might not be found.
+  -- | View one row in a data source.
   viewRow :: f Table.TableKey -> f Table.RowKey -> m (f (Maybe Table.Row))
-  viewRow tableKey rowKey = do
-    tables <- viewTableRows tableKey
-    pure $ Map.lookup <$> rowKey <*> tables
+  viewRow tableKeyF rowKeyF = do
+    rowsF <- viewRows $ Map.singleton <$> tableKeyF <*> ((:[]) <$> rowKeyF)
+    pure $ Map.lookup <$> rowKeyF <*> ((Map.!) <$> rowsF <*> tableKeyF)
 
-  -- | View all table rows in a data source.
-  viewTableRows :: f Table.TableKey -> m (f Table.Table)
+  -- | View multiple rows in a data source.
+  viewRows
+    :: f (Map.Map Table.TableKey [Table.RowKey])
+    -> m (f (Map.Map Table.TableKey Table.Table))
+  viewRows rowKeysMapF = do
+    tablesF <- viewTables $ Map.keys <$> rowKeysMapF
+    pure $ Map.intersectionWith restrictTable <$> tablesF <*> rowKeysMapF
 
-  -- | Set a single table row in a data source.
-  --
-  -- An existing row with the same key will be overwritten.
-  -- Any other processes watching for updates will be notified.
+  -- | View one table in a data source.
+  viewTable :: f Table.TableKey -> m (f (Table.Table))
+  viewTable tableKeyF = do
+    tablesF <- viewTables $ (:[]) <$> tableKeyF
+    pure $ (Map.!) <$> tablesF <*> tableKeyF
+
+  -- | View multiple tables in a data source.
+  viewTables :: f [Table.TableKey] -> m (f (Map.Map Table.TableKey Table.Table))
+
+  -- | Set one row in a data source.
   setRow :: f Table.TableKey -> f Table.RowKey -> f Table.Row -> m ()
-  setRow tableKey rowKey row = do
-    rows <- viewTableRows tableKey
-    setTableRows tableKey $ Map.insert <$> rowKey <*> row <*> rows
+  setRow tableKeyF rowKeyF rowF =
+    setRows $ Map.singleton <$> tableKeyF <*>
+      (Map.singleton <$> rowKeyF <*> rowF)
 
-  -- | Set multiple rows across multiple tables in a data source.
-  setManyRows :: f (Map.Map Table.TableKey Table.Table) -> m ()
-  setManyRows many = do
-    -- Convert the nested map into a flattened list.
-    let toRows = (\tableMap -> [
-          (tableKey, rowKey, row)
-          | (tableKey, table) <- zip (Map.keys tableMap) (Map.elems tableMap)
-          , (rowKey  , row  ) <- zip (Map.keys table   ) (Map.elems table   )
-          ]) <$> many
-    -- Sequence a list of operations to set each row.
-    void $ sequence =<< (escape $
-        (fmap . fmap) (\(a, b, c) -> setRow (pure a) (pure b) (pure c)) toRows)
+  -- | Set multiple rows in a data source.
+  setRows :: f (Map.Map Table.TableKey Table.Table) -> m ()
+  setRows newRowsMapF = do
+    tablesF <- viewTables $ Map.keys <$> newRowsMapF
+    setTables $ Map.union <$> newRowsMapF <*> tablesF
 
-  -- | Set a table in a data source to ONLY the given rows.
-  --
-  -- All existing rows in the table will be removed.
-  setTableRows :: f Table.TableKey -> f Table.Table -> m ()
+  -- | Set one table in a data source.
+  setTable :: f Table.TableKey -> f Table.Table -> m ()
+  setTable tableKeyF tableF = setTables $ Map.singleton <$> tableKeyF <*> tableF
 
-  -- | Remove a table row in a data source.
+  -- | Set multiple tables in a data source.
+  setTables :: f (Map.Map Table.TableKey Table.Table) -> m ()
+
+  -- | Remove one row from a data source.
   rmRow :: f Table.TableKey -> f Table.RowKey -> m ()
-  rmRow tableKey rowKey = do
-    rows <- viewTableRows tableKey
-    setTableRows tableKey $ Map.delete <$> rowKey <*> rows
+  rmRow tableKeyF rowKeyF =
+    rmRows $ Map.singleton <$> tableKeyF <*> ((:[]) <$> rowKeyF)
 
-  -- | Remove a table in a data source.
-  rmTableRows :: f Table.TableKey -> m ()
-  rmTableRows tableKey = setTableRows tableKey $ pure Map.empty
+  -- | Remove multiple rows from a data source.
+  rmRows :: f (Map.Map Table.TableKey [Table.RowKey]) -> m ()
+  rmRows rowKeysMapF = do
+    tablesF <- viewTables $ Map.keys <$> rowKeysMapF
+    setTables $ Map.intersectionWith restrictTable <$> tablesF <*> rowKeysMapF
 
-  -- | Run a function when a table row in a data source changes.
-  onChangeRow ::
-    f Table.TableKey -> f Table.RowKey -> f (Maybe Table.Row -> m ()) -> m ()
+  -- | Remove one table from a data source.
+  rmTable :: f Table.TableKey -> m ()
+  rmTable tableKeyF = setTable tableKeyF $ pure Map.empty
 
--- | User-facing class for storable data types.
---
--- Serialization and deserialization under one typeclass.
+  -- | Remove multiples table from a data source.
+  rmTables :: f [Table.TableKey] -> m ()
+  rmTables tableKeysF =
+    setTables $ Map.fromList . (flip zip $ repeat Map.empty) <$> tableKeysF
+
+  -- | Run a function when a row in a data source changes.
+  onChangeRow
+    :: f Table.TableKey -> f Table.RowKey -> f (Maybe Table.Row -> m ()) -> m ()
+
+-- | A storable datatype (can be serialized and deserialized via Generics).
 class    (Store.ToSDataType a, Store.FromSValues a) => Entity a where
 instance (Store.ToSDataType a, Store.FromSValues a) => Entity a where
+
+-- | Restrict a 'Table.Table' to only rows with given 'Table.RowKey's.
+restrictTable :: Table.Table -> [Table.RowKey] -> Table.Table
+restrictTable table rowKeys = Map.restrictKeys table (Set.fromList rowKeys)
