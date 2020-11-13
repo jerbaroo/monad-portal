2c545ab29b2d2d15b8ea1597984ef798fe529c47
jerbaroo <jerbaroo.work@pm.me>
2020-11-13 13:54:20 +0000

    Fix default.nix

--- default.nix
+++ default.nix
@@ -17,8 +17,34 @@
     telescope-server-api-types  = ./telescope-server-api-types;
   };
   shells = {
-    ghc   = ["demo-backend" "demo-common" "demo-frontend"];
-    ghcjs = [               "demo-common" "demo-frontend"];
+    ghc = [
+      "demo-backend"
+      "demo-common"
+      "demo-frontend"
+      "todolist-backend"
+      "todolist-common"
+      "todolist-frontend"
+      ## Telescope packages ##
+      "telescope"
+      "telescope-ds-file"
+      "telescope-ds-reflex-dom"
+      "telescope-server"
+      "telescope-server-api"
+      "telescope-server-api-types"
+    ];
+    ghcjs = [
+      "demo-common"
+      "demo-frontend"
+      "todolist-common"
+      "todolist-frontend"
+      ## Telescope packages ##
+      "telescope"
+      "telescope-ds-file"
+      "telescope-ds-reflex-dom"
+      "telescope-server"
+      "telescope-server-api"
+      "telescope-server-api-types"
+    ];
   };
   overrides = self: super: {
     servant-websockets = self.callHackage "servant-websockets" "2.0.0" {};
